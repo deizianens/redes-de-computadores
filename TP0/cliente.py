4a19f56c9937d3eebbea8f03f1b8fd41739a9034
Deiziane <deiziane_ns@hotmail.com>
2019-03-03 18:47:02 -0300

    almost there

--- TP0/cliente.py
+++ TP0/cliente.py
@@ -2,8 +2,9 @@ import socket
 import sys
 import struct
 import threading
+import time
 
-# usage: ./app-server.py <host> <port>
+# usage: ./cliente.py <host> <port>
 
 TIMEOUT = 15 # tempo que o server deve esperar para receber os dados (15 segundos)
 
@@ -20,12 +21,15 @@ class Cliente(threading.Thread):
     def run(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+        tv = struct.pack("ll", TIMEOUT, (1000*TIMEOUT)) # timeout
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDTIMEO, tv)
+        
         try:
             s.connect((self.host, self.port)) # Estabelece conexão.
         except socket.error as e:
             print('Falha de conexão: '+str(e))
             sys.exit()
-        
+
         s.send(msg)
 
         data = s.recv(1024) # recebe resposta do servidor
@@ -34,11 +38,22 @@ class Cliente(threading.Thread):
         s.close()
 
 
+def encodeMsg(msg):
+    if(msg[0] == '+'):
+        op = '1'+msg[1:]
+    elif (msg[0] == '-'):
+        op = '0'+msg[1:]
+
+    return op
+
+
 if __name__ == '__main__':
     HOST =  sys.argv[1]     # Host: endereço IP
     PORT = int(sys.argv[2]) # Port: porto 
-    m = input()
-    msg = m.encode()
+    msg = input()
+    msg = encodeMsg(msg)
+    
+    msg = msg.encode()
 
     for i in range(1):
         Cliente(i, HOST, PORT, *msg).start()
--- TP0/servidor.py
+++ TP0/servidor.py
@@ -4,13 +4,17 @@ import struct
 import threading
 import time
 
-# usage: ./app-server.py <host> <port>
+# usage: ./servidor.py
 
+CONTADOR = 0
 TIMEOUT = 15 # tempo que o server deve esperar para receber os dados (15 segundos)
 
 def server_main():
     # cria socket. argumentos: (IPV4, TCP oriented)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    tv = struct.pack("ll", TIMEOUT, (1000*TIMEOUT)) # timeout
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDTIMEO, tv)
 
     HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
     PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -23,35 +27,40 @@ def server_main():
     # escuta conexões (parâmetro: número de clientes por vez, no caso 1)
     s.listen(1)
 
-    #timeout
-    # timeval = struct.pack('ll', TIMEOUT, usec)
-    # s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeval)
-
     while True:
         print('Aguardando conexão!')
         conn, addr = s.accept() # abre conexão entre cliente e servidor
-        print('Conectado com ', addr)
-
         try:
             while True:
-                data = conn.recv(40) # recebe os dados (40 bits = 5 bytes)
+                data = conn.recv(40) # recebe os dados (40 bits = 5 bytes)                
+                
+                data = data.decode()
 
-                print ('Recebeu "%s"' % data)
+                result = decodeMsg(data)
+                result = result.encode()
 
                 if data:
-                    print ('Enviando dados de volta para cliente')
                     ''' 
                         Transmite mensagem de volta (Valor do contador)
                             - Representada como string (ASCII), seis caracteres de 0 a 999999
                     '''
-                    conn.sendall(data) 
+                    conn.sendall(result) 
                 else:
-                    print ('Sem mais dados de ', addr)
                     break
                 
         finally: # garante que conexão seja encerrada mesmo que haja erro
             # encerra conexão
             conn.close()
+
+
+def decodeMsg(data):
+    global CONTADOR
+    if(data[0] == 1):
+        CONTADOR = (CONTADOR + int(data[1:])) % 1000
+    else:
+        CONTADOR = (CONTADOR - int(data[1:])) % 1000
+    
+    return str(CONTADOR)
 
 
 if __name__ == '__main__':
