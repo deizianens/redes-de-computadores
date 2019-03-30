a01f557ef2b9acccb4ffcb3afc181d2d2c8bdf60
Deiziane <deiziane_ns@hotmail.com>
2019-03-30 19:12:57 -0300

    almost done, still missing tests and doc

--- TP1/dccnet.py
+++ TP1/dccnet.py
@@ -9,14 +9,73 @@ import socket
 import time
 import threading
 import binascii
-import struct
 
 MAX_DATA = 4096 # 512 bytes
 SOF = '0xcc'
 EOF = '0xcd'
+DLE = '0x1b'
 
 class Data:
-    
+    def __init__(self, data='', id=1, flags=0):
+        self.data = data
+        self.confirmed = False
+        self.id = id
+        self.flags = flags
+
+    def prepare_for_new_data(self):
+        self.confirmed = False
+        self.id = 1 if self.id == 0 else 0
+
+    @staticmethod
+    def _format_numbers(number, is_2_bytes=True):
+        pattern = '{:04x}' if is_2_bytes else '{:02x}'
+        return pattern.format(number)
+
+    def checksum(self):
+        data = 2 * SYNC
+        data += self._format_numbers(len(self.data))
+        data += self._format_numbers(0)
+        data += self._format_numbers(self.id, False)
+        data += self._format_numbers(self.flags, False)
+        data = binascii.unhexlify(data.encode()) + self.data
+
+        checksum = 0
+        pointer = 0
+        size = len(data)
+
+        while size > 1:
+            checksum += int(str('%02x' % (data[pointer],)) + str('%02x' % (data[pointer + 1],)), 16)
+            size -= 2
+            pointer += 2
+        if size:
+            checksum += data[pointer]
+
+        checksum = (checksum >> 16) + (checksum & 0xffff)
+        checksum += (checksum >> 16)
+
+        return (~checksum) & 0xFFFF
+
+    # encrypt
+    def encode16(self):
+        return binascii.hexlify(self.data)
+
+    # decrypt
+    def decode16(self, new_data):
+        self.data = binascii.unhexlify(new_data)
+
+    def get_frame(self):
+        formatted_length = self._format_numbers(len(self.data))
+        formatted_checksum = self._format_numbers(self.checksum())
+        formatted_id = self._format_numbers(self.id, False)
+        formatted_flags = self._format_numbers(self.flags, False)
+        encoded_data = self.encode16()
+        header = (formatted_length + formatted_checksum + formatted_id + formatted_flags).encode()
+        return header + encoded_data
+
+
+d_send = Data(id=0)
+d_rcv = Data()
+timeout = False
 
 
 '''
@@ -68,9 +127,14 @@ def init_server():
 
     if os.path.exists(OUTPUT):
         os.remove(OUTPUT)
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    except socket.error:
+        print("Erro ao criar socket!")
+        exit(2)
+    
     s.bind((IP, PORT))
     s.listen(1)
 
@@ -89,6 +153,77 @@ def init_server():
     1 byte | 1 byte  | 1 byte  | 2 bytes    | max 512 bytes  | 1 byte
 
 '''
+def receive_data(con, file_name):
+    id = 1 # id do ultimo quadro de dados recebido
+    data = ''
+    bef = ''
+    loop = 0
+
+    while True:
+        # espera até o recebimento de um SOF
+        sof = con.recv(2)
+        if sof.decode() != SOF:
+            continue
+        
+        id = con.recv(2)
+        d_rcv.id = int(id.decode(), base=16)
+
+        flags = con.recv(2)
+        d_rcv.flags = int(flags.decode(), base=16)
+
+        checksum_rcv = con.recv(4)
+        checksum_rcv = int(checksum_rcv.decode(), base=16)
+
+        try:
+            while (loop === 0):
+                aux = con.recv(2)
+                data += aux
+                while (aux.decode(), base=16) != EOF:
+                    bef = aux
+                    aux = con.recv(2) 
+                    data += aux
+                
+                if(aux.decode(), base=16) == EOF:
+                    if (bef != DLE) # fim de frame
+                        loop = 1
+            d_rcv.decode16(data)
+        except binascii.Error or UnicodeDecodeError:
+            print('Erro de conversão!')
+            continue
+
+        # verificando se o checksum está correto
+        if d_rcv.checksum() != checksum_rcv:
+            print('Erro no Checksum!')
+            continue
+
+        if d_rcv.flags == 128:
+            # treat received ACK
+            if d_rcv.id == d_send.id:
+                # print('ACK recebido!')
+                d_send.confirmed = True
+        else:
+            # treat new data
+            expected_id = 1 if id == 0 else 0
+            if d_rcv.id != expected_id:
+                # print('Retransmitindo dados e reenviando ACK.')
+                d_rcv.data = b''
+                d_rcv.flags = 128
+
+                con.send(SOF.encode())
+                con.send(d_rcv.get_frame())
+                continue
+
+            with open(file_name, 'ab') as file:
+                file.write(d_rcv.data)
+                d_rcv.data = b''
+                d_rcv.flags = 128
+                id = d_rcv.id
+
+                con.send(SOF.encode())
+                con.send(d_rcv.get_frame())
+
+
+
 def send_data(con, file_name):
     with open(file_name, 'rb') as file:
         line = file.read(MAX_DATA)
@@ -96,103 +231,27 @@ def send_data(con, file_name):
         while line:
             d_send.data = line
 
-            con.send(SYNC.encode())
-            con.send(SYNC.encode())
+            con.send(SOF.encode())
             con.send(d_send.get_frame())
-            log('Data sent from file ' + file_name)
 
-            global ack_timeout
-            ack_timeout = False
+            global timeout
+            timeout = False
 
             def handle_timeout():
-                global ack_timeout
-                ack_timeout = True
+                global timeout
+                timeout = True
 
-            # to measure time
             timer = threading.Timer(1, handle_timeout)
             timer.start()
             while True:
-                if ack_timeout:
-                    log('ACK not received.')
+                if timeout:
+                    print('Erro: ACK não foi recebido!')
                     break
 
                 if d_send.confirmed:
                     line = file.read(MAX_DATA)
                     d_send.prepare_for_new_data()
                     break
-
-
-def receive_data(con, file_name):
-    last_received_id = 1
-
-    while True:
-        # waiting for data
-        # finding SYNC expression
-        sync = con.recv(8)
-        if sync.decode() != SYNC:
-            # resynchronizing
-            continue
-
-        sync = con.recv(8)
-        if sync.decode() != SYNC:
-            # resynchronizing
-            continue
-
-        # collecting information in the string
-        length = con.recv(4)
-        data_to_receive.length = int(length.decode(), base=16)
-
-        received_checksum = con.recv(4)
-        received_checksum = int(received_checksum.decode(), base=16)
-
-        id = con.recv(2)
-        data_to_receive.id = int(id.decode(), base=16)
-
-        flags = con.recv(2)
-        data_to_receive.flags = int(flags.decode(), base=16)
-
-        try:
-            data = con.recv(2*data_to_receive.length)
-            data_to_receive.decode16(data)
-        except binascii.Error or UnicodeDecodeError:
-            log('Conversion error!')
-            continue
-
-        # verifying checksum value
-        if data_to_receive.checksum() != received_checksum:
-            log('Checksum error!')
-            continue
-
-        if data_to_receive.flags == 128:
-            # treat received ACK
-            if data_to_receive.id == d.id:
-                log('ACK received.')
-                d.confirmed = True
-
-        else:
-            # treat new data
-            expected_id = 1 if last_received_id == 0 else 0
-            if data_to_receive.id != expected_id:
-                log('Retransmission data, resending ACK.')
-                data_to_receive.data = b''
-                data_to_receive.flags = 128
-
-                con.send(SYNC.encode())
-                con.send(SYNC.encode())
-                con.send(data_to_receive.get_frame())
-                continue
-
-            with open(file_name, 'ab') as file:
-                file.write(data_to_receive.data)
-                log('Data written on file {}, sending ACK.'.format(file_name))
-                data_to_receive.data = b''
-                data_to_receive.flags = 128
-                last_received_id = data_to_receive.id
-
-                con.send(SYNC.encode())
-                con.send(SYNC.encode())
-                con.send(data_to_receive.get_frame())
-
 
 
 
